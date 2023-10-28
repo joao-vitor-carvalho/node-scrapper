e7559d3672319bb336838ced1bed3916ae0901d5
João Vitor Carvalho Barros <91700668+joao-vitor-carvalho@users.noreply.github.com>
2023-10-28 09:20:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { Builder, By } = require('selenium-webdriver');
  
+
+
+//connect to server for scrap
 const app = express();
 const port = 3000;
 app.get('/', async (request, response) => {
- // Web Scraping Code here
+ // Scraping Code 
  try {
    const data = await WebScrapingLocalTest();
    response.status(200).json(data);
@@ -17,13 +20,14 @@ app.get('/', async (request, response) => {
 app.listen(port, () => {
  console.log(`Example app listening at http://localhost:${port}`);
 });
- 
+
+//get allVideos to first page 
 async function WebScrapingLocalTest() {
  try {
    driver = await new Builder().forBrowser('chrome').build();
-   await driver.get('https://www.youtube.com/c/LambdaTest/videos');
+   await driver.get('http://webapplayers.com/inspinia_admin-v2.9.4/table_data_tables.html');
    const allVideos = await driver.findElements(
-     By.xpath('//*[@id="contents"]/ytd-rich-grid-row')
+     By.xpath('//*[@id="DataTables_Table_0"]/tbody/tr[1]')
    );
    return await getVideos(allVideos);
  } catch (error) {
@@ -34,17 +38,18 @@ async function WebScrapingLocalTest() {
 }
 
 //nao alterar função acima, trazendo vídeos
+//getVideos() split videos 
 
 async function getVideos(videos) {
     let videoDetails = [];
     try {
       for (const video of videos) {
-        const title = await video.findElement(By.xpath('//*[@id="video-title"]')).getText();
+        const title = await video.findElement(By.xpath('//*[@id="DataTables_Table_0"]/tbody/tr[1]')).getText();
         const views = await video
-          .findElement(By.xpath('//*[@id="metadata-line"]/span[1]'))
+          .findElement(By.xpath('//*[@id="DataTables_Table_0"]/tbody/tr[1]/td[1]'))
           .getText();
         const date = await video
-          .findElement(By.xpath(".//*[@id='metadata-line']/span[2]"))
+          .findElement(By.xpath('//*[@id="DataTables_Table_0"]/tbody/tr[1]/td[2]'))
           .getText();
         videoDetails.push({
           title: title ?? '',
@@ -60,4 +65,5 @@ async function getVideos(videos) {
 
 
 
-   //scrap finalizado e JSON gerado
+   //scrap finalizado e JSON gerado em videoDetails
+   //videoDetails returns 
